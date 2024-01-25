1d31be466439d27656b938df43e68a6571a9c869
Nikola Sirakov <156999301+nsirakow@users.noreply.github.com>
2024-01-25 23:03:02 +0200

    Create Tyres.java

---
+++ RawData/Tyres.java
@@ -0,0 +1,13 @@
+public class Tyres {
+    double tirePressure;
+    int tireAge;
+
+    public Tyres(double tirePressure, int tireAge) {
+        this.tirePressure = tirePressure;
+        this.tireAge = tireAge;
+    }
+
+    public double getTirePressure() {
+        return tirePressure;
+    }
+}
