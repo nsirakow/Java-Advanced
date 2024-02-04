8b12547de1d4ed33e9a0aeccb4fe3e5dbb979c28
Nikola Sirakov <156999301+nsirakow@users.noreply.github.com>
2024-02-04 23:50:51 +0200

    Update Person.java

--- PeopleCompare/Person.java
+++ PeopleCompare/Person.java
@@ -1,1 +1,27 @@
+public class Person implements Comparable<Person> {
+  
+    private String name;
+    private int age;
+    private String town;
+  
+    public Person(String name, int age, String town) {
+        this.name = name;
+        this.age = age;
+        this.town = town;
+    }
+    @Override
+    public int compareTo(Person other) {
+        // Compare names
+        int nameComparison = this.name.compareTo(other.name);
+        if (nameComparison != 0) {
+            return nameComparison;
+        }
+        // Compare ages
+        if (this.age != other.age) {
+            return Integer.compare(this.age, other.age);
+        }
+        // Compare towns
+        return this.town.compareTo(other.town);
+    }
+}
 
